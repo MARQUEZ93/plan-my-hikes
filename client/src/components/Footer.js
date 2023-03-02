e814ee6c0fdcf3d75fa107254e256ece9dcd7452
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-02 15:05:44 -0800

    simplify footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,27 +12,21 @@ const Footer = () => (
         <Grid divided inverted stackable>
           <Grid.Row>
             <Grid.Column width={3}>
-              <Header inverted as='h4' content='Home' />
+            <Header as='h4' inverted>
+                Plan My Hikes
+              </Header>
               <List link inverted>
-                <List.Item as='a'>About</List.Item>
                 <List.Item as='a'>Contact</List.Item>
+                <List.Item as='a'>Open Source Repository</List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={6}>
-              <Header inverted as='h4' content='Created by Alejandro E. Marquez' />
+              <Header inverted as='h4' 
+              content='Created by Alejandro E. Marquez' />
               <List link inverted>
-                <List.Item as='a'>Github</List.Item>
                 <List.Item as='a'>LinkedIn</List.Item>
                 <List.Item as='a'>Portfolio</List.Item>
               </List>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Header as='h4' inverted>
-                Open Source Repository
-              </Header>
-              <p>
-                Follow this script if you want to query & save ChatGPT results yourself! 
-              </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
