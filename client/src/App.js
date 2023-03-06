f0d0ed83930b888a874fa8928a56e85685d94345
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-05 16:09:29 -0800

    configure react router in most basic manner

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import 'semantic-ui-css/semantic.min.css';
+import { Link, Route, Routes, useSearchParams } from "react-router-dom";
 import Homepage from './components/Homepage';
+import NoMatch from './components/NoMatch';
+
 
 function App() {
   return (
-    <div>
-      <Homepage/>
-    </div>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<NoMatch />} />
+    </Routes>
   );
 };
 
