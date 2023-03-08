028dfae3c27a723b2fb6144ffb93f6bfab766331
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-08 16:40:27 -0600

    route on search submit

--- client/src/components/SearchHikes.js
+++ client/src/components/SearchHikes.js
@@ -1,23 +1,33 @@
 import _ from 'lodash';
 import React from 'react';
 import { Search, Grid, Header, Segment } from 'semantic-ui-react';
+import { Navigate } from "react-router-dom";
 
 const source = [
   {
-    title: 'Big Bend National Park (Texas, USA)'
+    title: 'Big Bend National Park (Texas, USA)',
+    name: 'Big Bend National Park'
   },
   {
-    title: 'Joshua Tree National Park (California, USA)'
+    title: 'Joshua Tree National Park (California, USA)',
+    name: 'Joshua Tree National Park'
   },
   {
-    title: 'Los Glaciares National Park (Argentina)'
-  },
+    title: 'Los Glaciares National Park (Argentina)',
+    name: 'Los Glaciares National Park'
+  }
 ];
 
 const initialState = {
   loading: false,
   results: [],
   value: '',
+  submit: false,
+  name: ''
+}
+
+function snakeCase(parkName){
+  return parkName.split(' ').join('_');
 }
 
 function exampleReducer(state, action) {
@@ -29,7 +39,7 @@ function exampleReducer(state, action) {
     case 'FINISH_SEARCH':
       return { ...state, loading: false, results: action.results };
     case 'UPDATE_SELECTION':
-      return { ...state, value: action.selection };
+      return { ...state, value: action.selection, submit: true, name: action.name };
 
     default:
       throw new Error()
@@ -40,7 +50,8 @@ function SearchHikes() {
   const [state, dispatch] = React.useReducer(exampleReducer, initialState)
   const { loading, results, value } = state
 
-  const timeoutRef = React.useRef()
+  const timeoutRef = React.useRef();
+
   const handleSearchChange = React.useCallback((e, data) => {
     clearTimeout(timeoutRef.current)
     dispatch({ type: 'START_SEARCH', query: data.value })
@@ -48,7 +59,7 @@ function SearchHikes() {
     timeoutRef.current = setTimeout(() => {
       if (data.value.length === 0) {
         dispatch({ type: 'CLEAN_QUERY' })
-        return
+        return redirect("/login");
       }
 
       const re = new RegExp(_.escapeRegExp(data.value), 'i')
@@ -59,22 +70,28 @@ function SearchHikes() {
         results: _.filter(source, isMatch),
       })
     }, 300)
-  }, [])
+  }, []);
+
   React.useEffect(() => {
     return () => {
       clearTimeout(timeoutRef.current)
     }
-  }, [])
+  }, []);
 
+  if (state.submit){
+    console.log(state);
+    console.log(snakeCase(state.name));
+    return <Navigate to={`/parks/${snakeCase(state.name.toLowerCase())}`} />;
+  }
   return (
     <Grid textAlign='center'>
       <Grid.Column>
         <Search
           loading={loading}
           placeholder='Search...'
-          onResultSelect={(e, data) =>
-            dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title})
-          }
+          onResultSelect={(e, data) => {
+            dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title, name: data.result.name})
+          }}
           onSearchChange={handleSearchChange}
           results={results}
           value={value}
