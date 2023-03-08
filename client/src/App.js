5e7f2626ab112d8ffb74252b96e8c62e9720dc46
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-07 18:44:56 -0600

    add route for park

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import 'semantic-ui-css/semantic.min.css';
 import { Link, Route, Routes, useSearchParams } from "react-router-dom";
 import Homepage from './components/Homepage';
 import NoMatch from './components/NoMatch';
+import Park from './components/Park';
 
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/parks/:name" element={<Park />} />
         <Route path="*" element={<NoMatch />} />
     </Routes>
   );
