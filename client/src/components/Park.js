4b5734981f3ed47b018d99fa2a28e0aa5296c2b4
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-07 23:17:57 -0600

    get basic Park component

--- client/src/components/Park.js
+++ client/src/components/Park.js
@@ -1,36 +1,53 @@
 import React,{useState,useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import NoMatch from './NoMatch';
 
 function Park() {
-  const [data,setData]=useState([]);
-  const getData=()=>{
-    fetch('../data.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
+
+    const { name } = useParams();
+
+    const [ data, setData ] = useState( [] );
+
+    const getData=()=>{
+        fetch('../data.json'
+        , {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(function(response){
+            return response.json();
+        }).then(function(myJson) {
+            setData(myJson);
+        });
+    };
+
+    useEffect(()=>{
+        getData();
+    },[]);
+
+    if (data && data[0]){
+        const parkData = data[0][name];
+        if (!parkData){
+            return <NoMatch />;
+        }
+        return (
+            <div className="App">
+                <p>Park Name:{parkData.name}</p>
+                <p>Location:{parkData.location}</p>
+                <p>Schedule:{parkData.schedule}</p>
+                <p>Hidden Gem:{parkData.hidden_gem}</p>
+                <p>Unique:{parkData.unique}</p>
+                <p>Best:{parkData.best}</p>
+                <p>Camping:{parkData.camping}</p>
+            </div>
+        );
     }
-    )
-      .then(function(response){
-        console.log(response);
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
-  useEffect(()=>{
-    getData();
-  },[]);
-  return (
-    <div className="App">
-        <p>HERE</p>
-     {
-       data && data.length>0
-     }
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>Loading...</p>
+        </div>
+    );
 }
 
 export default Park;
