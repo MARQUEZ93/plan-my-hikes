dc3126c2e0e6d1330d2f1727a8fd014559bcf9bf
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-03-11 18:01:53 -0600

    refactor routing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import 'semantic-ui-css/semantic.min.css';
-import { Link, Route, Routes, useSearchParams } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Homepage from './components/Homepage';
 import NoMatch from './components/NoMatch';
 import Park from './components/Park';
-
+import SiteHeader from './components/SiteHeader';
+import Footer from './components/Footer';
+import HomepageHeading from './components/HomepageHeading';
 
 function App() {
   return (
-    <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/parks/:name" element={<Park />} />
-        <Route path="*" element={<NoMatch />} />
-    </Routes>
+    <BrowserRouter style={{backgroundColor: '#F0F0F0'}}>
+      <SiteHeader />
+      <Routes style={{backgroundColor: '#F0F0F0'}}>
+          <Route path="/" element={<HomepageHeading />} />
+          <Route path="/parks/:name" element={<Park />} />
+          <Route path="*" element={<NoMatch />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
