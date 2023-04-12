82b6be1f07500eae17194c3335cc87152c40174b
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-04-12 18:29:40 -0400

    fix favicon + remove some stuff

--- client/src/components/SiteHeader.js
+++ client/src/components/SiteHeader.js
@@ -5,33 +5,15 @@ import {
 } from 'semantic-ui-react';
 import MainMenu from './MainMenu';
 
-const DesktopHeader = ({isMobile}) => (
-          <Segment
-            inverted
-            textAlign='center'
-            style={{backgroundColor: '#F0F0F0', height: '100%'}}
-            vertical
-          >
-            <MainMenu/>
-          </Segment>
-);
-
-const MobileHeader = ({isMobile}) => (
+const SiteHeader = ({isMobile=false}) => (
   <Segment
-          textAlign='center'
-          vertical
-        >
-          <Container><MainMenu isMobile={isMobile} /></Container>
-        </Segment>
+    inverted
+    textAlign='center'
+    style={{backgroundColor: '#F0F0F0', height: '100%'}}
+    vertical
+  >
+    <MainMenu isMobile/>
+  </Segment>
 );
-
-const SiteHeader = ({isMobile=false}) => {
-  console.log(isMobile);
-  if (isMobile){
-    return <MobileHeader isMobile={isMobile}/>;
-  } else {
-    return <DesktopHeader />;
-  }
-};
 
 export default SiteHeader;
