c000adbe4fe96939a7d7f38da0696a7b113a40a9
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-04-19 22:02:13 -0500

    install openai

--- backend/server.js
+++ backend/server.js
@@ -19,23 +19,14 @@ const pool = new Pool({
 
 app.use(express.static('public'));
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
 app.get('/parks/:route', async (req, res) => {
   const route = req.params.route;
   pool.query('SELECT name, route, location, tough, gem, schedule, busy, camping, tips, best FROM parks WHERE route = $1', [route], (err, result) => {
     if (err) {
-      console.error('Error executing query', err);
       res.status(500).send('Error executing query');
     } else if (result.rows.length === 0) {
-      console.error('Park not found: ' + route);
       res.status(404).send('Park not found');
     } else {
-      console.log('Park found: ' + route);
       const park = result.rows[0];
       res.send(park);
     }
