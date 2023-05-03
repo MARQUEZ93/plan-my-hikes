19afdc2e045633896221e5c33c8e92e9f42eda36
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-05-03 18:36:16 -0500

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const pool = new Pool({
 
 pool.on("connect", (client) => {
   client
-    .query("CREATE TABLE IF NOT EXISTS parks (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, location VARCHAR(255) NOT NULL, route VARCHAR(255) NOT NULL UNIQUE, tips TEXT NOT NULL, best TEXT NOT NULL, hidden_gem TEXT NOT NULL, schedule TEXT NOT NULL, camping TEXT NOT NULL, busy TEXT NOT NULL, tough TEXT NOT NULL, created_at TIMESTAMP DEFAULT NOW()")
+    .query("CREATE TABLE IF NOT EXISTS parks (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, location VARCHAR(255) NOT NULL, route VARCHAR(255) NOT NULL UNIQUE, tips TEXT NOT NULL, best TEXT NOT NULL, hidden_gem TEXT NOT NULL, schedule TEXT NOT NULL, camping TEXT NOT NULL, busy TEXT NOT NULL, tough TEXT NOT NULL, created_at TIMESTAMP DEFAULT NOW() )")
     .catch((err) => console.error(err));
 });
 
