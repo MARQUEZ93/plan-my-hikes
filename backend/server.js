61bb6be9a9d59a5e93c63ffdea40ef70a2c70b3b
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2023-05-03 18:16:30 -0500

    init db

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,12 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD
 });
 
+pool.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS parks (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, location VARCHAR(255) NOT NULL, route VARCHAR(255) NOT NULL UNIQUE, tips TEXT NOT NULL, best TEXT NOT NULL, hidden_gem TEXT NOT NULL, schedule TEXT NOT NULL, camping TEXT NOT NULL, busy TEXT NOT NULL, tough TEXT NOT NULL, created_at TIMESTAMP DEFAULT NOW()")
+    .catch((err) => console.error(err));
+});
+
 app.get('/api/parks/:route', async (req, res) => {
   const route = req.params.route;
   pool.query('SELECT name, route, location, tough, hidden_gem, schedule, busy, camping, tips, best FROM parks WHERE route = $1', [route], (err, result) => {
